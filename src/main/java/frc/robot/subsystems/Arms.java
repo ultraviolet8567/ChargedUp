cf568e1c3321e89bec6d763ae24e2dd446780a01
murham <murham43@gmail.com>
2023-02-21 16:32:56 -0500

    update to WPILib version 2023.4.1 (hopefully)

--- src/main/java/frc/robot/subsystems/Arms.java
+++ src/main/java/frc/robot/subsystems/Arms.java
@@ -1,3 +1,7 @@
-public class Arms {
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arms extends SubsystemBase {
     
 }
--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,3 +1,7 @@
-public class Vision {
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
     
 }
