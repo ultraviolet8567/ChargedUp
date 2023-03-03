776d8fb9353823be29fa459945f8a1a688534116
TaoTaoBamBam <srta.taotaobambam@gmail.com>
2023-03-03 15:45:46 -0500

    updated to 2023.4.1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "charged-up-2023-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "83c3fd62064729b5ba38553470b2871c3789e736";
-  public static final String GIT_DATE = "2023-02-22 12:58:31 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "bb5526b90d0921ae385d2b4eb08d6bb69f8a21c6";
+  public static final String GIT_DATE = "2023-03-01 14:57:24 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-03-01 14:56:10 EST";
-  public static final long BUILD_UNIX_TIME = 1677700570517L;
+  public static final String BUILD_DATE = "2023-03-03 15:32:11 EST";
+  public static final long BUILD_UNIX_TIME = 1677875531162L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
