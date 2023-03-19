949776e6606ba62cfdae8d6a9715ac2eadcb3fb6
Shmorgendiggle2 <93840614+Shmorgendiggle2@users.noreply.github.com>
2023-03-19 10:57:28 -0400

    made auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "ChargedUp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 44;
-  public static final String GIT_SHA = "b78d367f86b24b5e92437bab01248026f7eb37dd";
-  public static final String GIT_DATE = "2023-03-17 19:16:51 EDT";
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "3e17da14fa99a2085f2152546f1f8d45fd711382";
+  public static final String GIT_DATE = "2023-03-18 15:55:40 EDT";
   public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2023-03-17 21:47:07 EDT";
-  public static final long BUILD_UNIX_TIME = 1679104027961L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2023-03-18 18:30:02 EDT";
+  public static final long BUILD_UNIX_TIME = 1679178602083L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/commands/AutoCommands/AutoDriveOut.java
@@ -0,0 +1,59 @@
+package frc.robot.commands.AutoCommands;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.commands.SwerveTeleOp;
+import frc.robot.subsystems.Swerve;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Timer;
+
+public class AutoDriveOut extends CommandBase {
+    private Swerve swerve;
+    private Timer timer;
+    private SwerveTeleOp runSwerve;
+
+    public AutoDriveOut(Swerve swerve) {
+        this.swerve = swerve;
+    }
+  
+    @Override 
+    public void initialize() {
+        timer = new Timer();
+        timer.start();
+
+        runSwerve = new SwerveTeleOp(
+            swerve,
+            () -> 2.0,
+            () -> 0.0,
+            () -> 0.0,
+            () -> false,
+            () -> true);
+        runSwerve.initialize();
+    }
+
+    @Override
+    public void execute() {
+        
+        
+        if (timer.get() > 1) { 
+            runSwerve.execute();
+            // chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(2.0, 0.0, 0.0, new Rotation2d(0));
+        }
+        Logger.getInstance().recordOutput("timer", timer.get());
+    }
+    public void end(boolean interrupted) {
+        // chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(0.0, 0.0, 0.0, new Rotation2d(0));
+        timer.stop();
+        runSwerve.end(false);
+    }
+    public boolean isFinished() {
+        return timer.get() >= 15;
+    }
+    public boolean runsWhenDisabled() {
+        return false;
+    }
+}
