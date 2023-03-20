fb9473419907d94eb4cdecb2539af159aa127f3d
flame-cat <kaldraganov05@gmail.com>
2023-03-20 00:10:38 -0400

    Add untracked files to gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "charged-up-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "4defb48dace3618b9b36e64fc64e8ad3a3a3a7b4";
-  public static final String GIT_DATE = "2023-03-19 17:00:24 EDT";
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "6aedaad2da5aedbc44f6834ea7fe3c874d7b6811";
+  public static final String GIT_DATE = "2023-03-19 23:10:48 EDT";
   public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2023-03-19 22:58:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1679281101056L;
+  public static final String BUILD_DATE = "2023-03-20 00:02:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1679284954511L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
