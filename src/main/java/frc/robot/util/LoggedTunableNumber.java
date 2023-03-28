95cf4db0275e1c644f4812a015d573c3159a8f7b
flame-cat <kaldraganov05@gmail.com>
2023-03-28 17:26:55 -0400

    Move `LoggedTunableNumber.java` to Shuffleboard

--- src/main/java/frc/robot/util/LoggedTunableNumber.java
+++ src/main/java/frc/robot/util/LoggedTunableNumber.java
@@ -1,8 +1,9 @@
 package frc.robot.util;
 
-import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
-
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.Constants;
 import frc.robot.Constants.Mode;
 
@@ -11,89 +12,61 @@ import frc.robot.Constants.Mode;
  * value not in dashboard.
  */
 public class LoggedTunableNumber {
-  private static final String tableKey = "TunableNumbers";
-  private static final boolean tuningMode = Constants.currentMode == Mode.TUNING;
+    private static final ShuffleboardTab table = Shuffleboard.getTab("TunableNumbers");
+    private static final boolean tuningMode = Constants.currentMode == Mode.TUNING;
 
-  private final String key;
-  private boolean hasDefault = false;
-  private double defaultValue;
-  private LoggedDashboardNumber dashboardNumber;
-  private double lastHasChangedValue;
+    private final String key;
+    private final GenericEntry entry;
+    private final double defaultValue;
 
-  /**
-   * Create a new LoggedTunableNumber
-   * 
-   * @param dashboardKey Key on dashboard
-   */
-  public LoggedTunableNumber(String dashboardKey) {
-    this.key = tableKey + "/" + dashboardKey;
-  }
-
-  /**
-   * Create a new LoggedTunableNumber with the default value
-   * 
-   * @param dashboardKey Key on dashboard
-   * @param defaultValue Default value
-   */
-  public LoggedTunableNumber(String dashboardKey, double defaultValue) {
-    this(dashboardKey);
-    initDefault(defaultValue);
-
-    if (tuningMode) {
-      Shuffleboard.getTab("TunableNumbers");
-    }
-  }
-
-  /**
-   * Set the default value of the number. The default value can only be set once.
-   * 
-   * @param defaultValue The default value
-   */
-  public void initDefault(double defaultValue) {
-    if (!hasDefault) {
-      hasDefault = true;
-      this.defaultValue = defaultValue;
-      if (tuningMode) {
-        dashboardNumber = new LoggedDashboardNumber(key, defaultValue);
-      }
-    }
-  }
-
-  /**
-   * Get the current value, from dashboard if available and in tuning mode.
-   * 
-   * @return The current value
-   */
-  public double get() {
-    if (!hasDefault) {
-      return 0.0;
-    } else {
-      return tuningMode ? dashboardNumber.get() : defaultValue;
-    }
-  }
-
-  /**
-   * Get the default value for the number that has been set
-   * 
-   * @return The default value
-   */
-  public double getDefault() {
-    return defaultValue;
-  }
-
-  /**
-   * Checks whether the number has changed since our last check
-   * 
-   * @return True if the number has changed since the last time this method was called, false
-   *         otherwise
-   */
-  public boolean hasChanged() {
-    double currentValue = get();
-    if (currentValue != lastHasChangedValue) {
-      lastHasChangedValue = currentValue;
-      return true;
+    /**
+    * Create a new LoggedTunableNumber
+    *
+    * @param dashboardKey Key on dashboard
+    */
+    public LoggedTunableNumber(String dashboardKey) {
+        this(dashboardKey, 0);
     }
 
-    return false;
-  }
+    /**
+    * Create a new LoggedTunableNumber with the default value
+    * 
+    * @param dashboardKey Key on dashboard
+    * @param defaultValue Default value
+    */
+    public LoggedTunableNumber(String dashboardKey, double defaultValue) {
+        key = dashboardKey;
+        entry = table.add(key, 0).withWidget(BuiltInWidgets.kTextView)
+            .withSize(2, 1)
+            .getEntry();
+        
+        this.defaultValue = defaultValue;
+    }
+
+    /**
+    * Get the current value, from dashboard if available and in tuning mode.
+    * 
+    * @return The current value
+    */
+    public double get() {
+        return tuningMode ? entry.getDouble(defaultValue) : defaultValue;
+    }
+
+    /**
+    * Get the default value for the number that has been set
+    * 
+    * @return The default value
+    */
+    public double getDefault() {
+        return defaultValue;
+    }
+
+    /**
+     * Get the key for the record on the dashboard
+     * 
+     * @return The dashboard key
+     */
+    public String getKey() {
+        return key;
+    }
 }
