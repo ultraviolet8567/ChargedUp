2afe82ff9f346dd3abd1866b849557ee0ff11ad4
Jase Watson <25watsonj@wpsraiders.org>
2023-04-29 16:18:11 -0400

    this does not work

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
@@ -9,27 +11,38 @@ public class Lights extends SubsystemBase{
     private AddressableLED m_led;
     private AddressableLEDBuffer m_ledBuffer;
 
-    private static final int length = 3;
+    private static final int length = 5;
 
     public Lights() {
-        System.out.println("sfd");
-        // PWM port 9
-        // Must be a PWM header, not MXP or DIO
-        m_led = new AddressableLED(4);
-        // Reuse buffer
-        // Default to a length of 60, start empty output
-        // Length is expensive to set, so only set it once, then just update data
-        m_ledBuffer = new AddressableLEDBuffer(length);
-        m_led.setLength(m_ledBuffer.getLength());
-        for (int i = 0; i < length; i++) {
-            // Sets the specified LED to the RGB values for red
-            m_ledBuffer.setLED(i, Color.kOrangeRed);;
-        }
-        // Set the data
-        m_led.setData(m_ledBuffer);
-        m_led.start();
+            // PWM port 9
+
+    // Must be a PWM header, not MXP or DIO
+
+    m_led = new AddressableLED(0);
 
 
+    // Reuse buffer
+
+    // Default to a length of 60, start empty output
+
+    // Length is expensive to set, so only set it once, then just update data
+
+    m_ledBuffer = new AddressableLEDBuffer(length);
+
+    m_led.setLength(m_ledBuffer.getLength());
+
+
+    // Set the data
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+        // Sets the specified LED to the RGB values for red
+        m_ledBuffer.setRGB(i, 255, 0, 0);
+    }
+    m_led.setData(m_ledBuffer);
+
+    m_led.start();
+    
+    
+        
         //Section section = Section.FULL;
 
         // for (int i = section.start(); i < section.end(); i++) {
@@ -37,7 +50,9 @@ public class Lights extends SubsystemBase{
         //     m_ledBuffer.setLED(i, Color.kOrangeRed);;
         // }
 
-        m_led.setData(m_ledBuffer);
+       // m_led.setData(m_ledBuffer);
+
+       Logger.getInstance().recordOutput("Lights/Color", "hello");
     }
     /*
     private static enum Section {
