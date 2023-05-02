98aa322518ec5f9f01bb2ffa5b15b3ea3da55c37
Jase Watson <25watsonj@wpsraiders.org>
2023-05-02 16:12:58 -0400

    change the colors of the lights

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -13,8 +13,10 @@ public class Lights extends SubsystemBase{
 
     private static final int length = 5;
 
+    private static Color ledColor = Color.kViolet;
+    private static boolean lowBattery = false;
+
     public Lights() {
-        // PWM port 9
         // Must be a PWM header, not MXP or DIO
         leds = new AddressableLED(0);
 
@@ -23,20 +25,23 @@ public class Lights extends SubsystemBase{
         // Length is expensive to set, so only set it once, then just update data
         buffer = new AddressableLEDBuffer(length);
         leds.setLength(length);
-        leds.setData(buffer);
-        leds.start();
+
     }
 
     public void periodic() {
-        Logger.getInstance().recordOutput("Lights/Color", "Red");
+        //Logger.getInstance().recordOutput("Lights/Color", "Red");
 
         // Set the data
-        for (var i = 0; i < buffer.getLength(); i++) {
-            // Sets the specified LED to the RGB values for red
-            buffer.setLED(i, Color.kRed);
+        if(lowBattery){
+          setBufferColor(Section.BOTTOM.start(), Section.BOTTOM.end(), Color.kOrangeRed);
+          setBufferColor(Section.BOTTOM.start(), Section.BOTTOM.end(), ledColor);
+        }else{
+          setBufferColor(Section.FULL.start(), Section.FULL.end(), ledColor);
         }
-
         leds.setData(buffer);
+        leds.start();
+
+        //leds.setData(buffer);
         
         //Section section = Section.FULL;
 
@@ -48,26 +53,59 @@ public class Lights extends SubsystemBase{
         // m_led.setData(m_ledBuffer);
     }
 
-    /*
+    
     private static enum Section {
         BOTTOM,
-        BACK_UPPER,
-        BACK,
-        FRONT,
+        UPPER,
         FULL;
 
         private int start() {
-            switch(this){
-                case BOTTOM:
-                    return 0;
-                default:
-                    return 0;
-            }
+          switch(this) {
+              case BOTTOM:
+                return 0;
+              case UPPER:
+              //this is a place holder value
+                return 3;
+              default:
+                return 0;
+          }
         }
 
         private int end() {
-            return length;
+          switch(this) {
+            case BOTTOM:
+            //this is too
+              return 2;
+            case UPPER:
+              return length - 1;
+            default:
+              return length - 1;
+          }
         }
     }
-    */
+
+    private void setBufferColor(int start, int end, Color color){
+      for (var i = start; i < end; i++) {
+        // Sets the specified LED to the RGB values for red
+        buffer.setLED(i, color);
+      }
+    }
+
+    public void setLowBattery(boolean lowBat){
+      lowBattery = lowBat;
+    }
+    public void disabled(){
+      ledColor = Color.kViolet;
+    }
+    public void auto(){
+      //change this to rainbow
+      ledColor = Color.kOrange;
+    }
+    public void cube(){
+      ledColor = Color.kPurple;
+    }
+    public void objectPicked(){
+      ledColor = Color.kGreen;
+    }
+
 }
